274eb1ef28277796eb56952d702e5069cd0510af
maulikvaghela <maulik.vagh@gmail.com>
2022-05-30 00:43:16 -0400

    initial commit

---
+++ src/Carousel.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+
+it("works when you click on the right arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+
+
+expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+it('should match snapshot', () => {
+  const { asFragment } = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+})
+
+// it('should match snapshot', () => {
+//   const { asFragment } = render(<Carousel />);
+//   expect(asFragment()).tomatchSnapshot();
+// })
